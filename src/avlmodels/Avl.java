273f1b141148667b33f150c031a69434b66d21f7
Rodrigo M <rodrigo.medina.neri@gmail.com>
2018-05-04 01:14:55 -0500

    Working Printing AVL

---
+++ src/avlmodels/Avl.java
@@ -0,0 +1,106 @@
+package avlmodels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Avl<T extends Comparable<T>> {
+
+    private Nodo<T> raiz;
+
+
+    public Nodo<T> getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(Nodo<T> raiz) {
+        this.raiz = raiz;
+    }
+
+    public void insertaElemento(T elemento){
+        raiz= insertaRecursiva(elemento,raiz);
+    }
+
+    private Nodo<T> insertaRecursiva(T elemento, Nodo<T> raiz){
+        if(raiz==null) {
+            raiz= new Nodo<T>(elemento);
+        }
+        else if(elemento.compareTo(raiz.getElemento())<0) {
+            raiz.setIzquierdo(insertaRecursiva(elemento, raiz.getIzquierdo()));
+            if(altura(raiz.getIzquierdo())-altura(raiz.getDerecho())==2) {
+                if(elemento.compareTo(raiz.getIzquierdo().getElemento())<0) {
+                    raiz= rotarALaDerechaSimple(raiz);
+                }else {
+                    raiz= rotacionDobleALaDerecha(raiz);
+                }
+            }
+        }
+        else if(elemento.compareTo(raiz.getElemento())>0) {
+            raiz.setDerecho(insertaRecursiva(elemento, raiz.getDerecho()));
+            if(altura(raiz.getIzquierdo())-altura(raiz.getDerecho())==-2) {
+                if(elemento.compareTo(raiz.getDerecho().getElemento())>0) {
+                    raiz= rotarALaIzquierdaSimple(raiz);
+                }else {
+                    raiz= rotacionDobleALaIzquierda(raiz);
+                }
+            }
+        }
+        int altura= max(altura(raiz.getIzquierdo()),altura(raiz.getDerecho()));
+        raiz.setAltura(altura+1);
+        return raiz;
+    }
+
+    private Nodo<T> rotarALaIzquierdaSimple(Nodo<T> raiz)
+    {
+        System.out.println("Roto simple a la izquierda");
+        Nodo<T>  temp = raiz.getDerecho();
+        raiz.setDerecho( temp.getIzquierdo());
+        temp.setIzquierdo(raiz);
+        raiz.setAltura(max( altura( raiz.getIzquierdo() ), altura( raiz.getDerecho() ) ) + 1);
+        temp.setAltura(max( altura( temp.getDerecho() ), raiz.getAltura() ) + 1);
+        return temp;
+    }
+
+    private Nodo<T> rotarALaDerechaSimple(Nodo<T> raiz)
+    {
+        System.out.println("Roto simple a la derecha");
+
+        Nodo<T> temp = raiz.getIzquierdo();
+        raiz.setIzquierdo(temp.getDerecho());
+        temp.setDerecho(raiz);
+        raiz.setAltura(max( altura( raiz.getIzquierdo() ), altura( raiz.getDerecho() ) ) + 1);
+        temp.setAltura(max( altura( temp.getIzquierdo() ), raiz.getAltura() ) + 1);
+        return temp;
+    }
+    private Nodo<T> rotacionDobleALaDerecha(Nodo<T> raiz)
+    {
+        System.out.println("Roto doble a la derecha");
+
+        raiz.setIzquierdo(rotarALaIzquierdaSimple( raiz.getIzquierdo() ));
+        return rotarALaDerechaSimple( raiz );
+    }
+    private Nodo<T> rotacionDobleALaIzquierda(Nodo<T> raiz)
+    {
+        System.out.println("Roto doble a la izquierda");
+
+        raiz.setDerecho(rotarALaDerechaSimple( raiz.getDerecho() ));
+        return rotarALaIzquierdaSimple( raiz );
+    }
+
+
+    private int max(int a,int b) {
+        if(a>b) {
+            return a;
+        }else {
+            return b;
+        }
+    }
+    private int altura(Nodo<T> nodo) {
+        if(nodo==null) {
+            return -1;
+        }else {
+            return nodo.getAltura();
+        }
+    }
+
+}
+
---
+++ src/avltreeui/Controller.java
@@ -0,0 +1,253 @@
+package avltreeui;
+
+import avlmodels.Avl;
+import avlmodels.Nodo;
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextBoundsType;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+
+    @FXML private TextField etNodeValue;
+    @FXML private Pane mainPane;
+
+    private float circleSize = 25.0f;
+
+    private ArrayList<Integer> elements = new ArrayList<>();
+
+    private Avl<Integer> avlTree = new Avl<>();
+
+    private void showMessage(String message){
+        JOptionPane.showMessageDialog(null, message);
+    }
+
+    @FXML
+    private void onAddButtonClicked(){
+
+        int nodeValue;
+
+        try{
+
+            nodeValue = Integer.parseInt(etNodeValue.getText());
+
+            Long res = elements.stream().filter(n -> n == nodeValue).count();
+
+            if(res == 0){
+
+                cleanMainPane();
+                elements.add(nodeValue);
+                addElementToAvl(nodeValue);
+                drawAvl();
+
+            } else showMessage("Ese elemento ya existe en el AVL");
+
+
+        } catch(Exception ex){
+
+            showMessage("El valor debe de ser numérico");
+
+        }
+
+    }
+
+    private void addElementToAvl(int element){
+
+        avlTree.insertaElemento(element);
+
+    }
+
+    private void drawNodeCircle(String element, double xPosition, double yPosition){
+
+        Circle circle = createCircle();
+        Text text = createText(element);
+
+        Group group = new Group(circle);
+
+        StackPane stack = new StackPane();
+        stack.setLayoutX(xPosition);
+        stack.setLayoutY(yPosition);
+        stack.getChildren().addAll(group, text);
+
+        mainPane.getChildren().add(stack);
+
+        double extraWidth = 10.0f;
+        mainPane.setPrefWidth(mainPane.getPrefWidth()+ extraWidth);
+
+    }
+
+    private Text createText(String nodeValue){
+
+        final Text text = new Text(nodeValue);
+        text.setFont(new Font(12));
+        text.setBoundsType(TextBoundsType.VISUAL);
+
+        return text;
+
+    }
+
+    private Circle createCircle(){
+
+        Circle circle = new Circle();
+
+        circle.setRadius(circleSize);
+        circle.setStroke(Color.DEEPSKYBLUE);
+        circle.setStrokeWidth(2);
+        circle.setStrokeType(StrokeType.INSIDE);
+        circle.setFill(Color.AZURE);
+        circle.relocate(0, 0);
+
+
+        return circle;
+
+    }
+
+    private void cleanMainPane(){
+
+        mainPane.getChildren().clear();
+
+    }
+
+    private void drawAvl(){
+
+        // Bien 2
+//        drawNodeCircle("1", circleSize*1, 0.0f);
+//
+//        drawNodeCircle("1", 0.0f, circleSize*2);
+//        drawNodeCircle("2", circleSize*2, circleSize*2);
+
+
+        // Bien 4
+//        drawNodeCircle("1", circleSize*3, 0.0f);
+//
+//        drawNodeCircle("1", circleSize*1, circleSize*2);
+//        drawNodeCircle("2", circleSize*5, circleSize*2);
+//
+//        drawNodeCircle("1", 0.0f, circleSize*4);
+//        drawNodeCircle("2", circleSize*2, circleSize*4);
+//        drawNodeCircle("3", circleSize*4, circleSize*4);
+//        drawNodeCircle("4", circleSize*6, circleSize*4);
+
+        // Bien 8
+//        drawNodeCircle("1", circleSize*7, 0.0f);
+//
+//        drawNodeCircle("1", circleSize*3, circleSize*2);
+//        drawNodeCircle("2", circleSize*11, circleSize*2);
+//
+//        drawNodeCircle("1", circleSize*1, circleSize*4);
+//        drawNodeCircle("2", circleSize*5, circleSize*4);
+//        drawNodeCircle("3", circleSize*9, circleSize*4);
+//        drawNodeCircle("4", circleSize*13, circleSize*4);
+//
+//        drawNodeCircle("1", 0.0f, circleSize*6);
+//        drawNodeCircle("2", circleSize*2, circleSize*6);
+//        drawNodeCircle("3", circleSize*4, circleSize*6);
+//        drawNodeCircle("4", circleSize*6, circleSize*6);
+//        drawNodeCircle("1", circleSize*8, circleSize*6);
+//        drawNodeCircle("2", circleSize*10, circleSize*6);
+//        drawNodeCircle("3", circleSize*12, circleSize*6);
+//        drawNodeCircle("4", circleSize*14, circleSize*6);
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("/////////////////////Nueva Insercion///////////////////");
+
+        Nodo root = avlTree.getRaiz();
+
+        if(root == null) {
+            // Como sólo agregas, nunca debería entrar acá
+            JOptionPane.showMessageDialog(null, "El Árbol está vacío");
+            return;
+        }
+
+        // Altura del árbol
+        int height = root.getAltura()+1;
+
+        // Ancho del árbol, es la máxima cantidad de posibles 'hojas' del árbol
+        int width = (int)Math.pow(2, height-1);
+
+        System.out.println("Altura -> "+height);
+        System.out.println("Anchura -> "+width);
+
+        // Variables para el ciclo
+        List<Nodo> current = new ArrayList<>(1);
+        List<Nodo> next = new ArrayList<>(2);
+
+        current.add(root);
+
+        // Cantidad de elements a usar en la siguiente iteración del ciclo
+        // Cada iteración será el doble de elements
+        int elements = 1;
+
+        // Un acumulador que cuenta cada que recorres un nivel del arbol
+        int acum = 0;
+
+        // Posiciones en pixeles de los Circulos
+        float actualPositionX = 0;
+        float actualPositionY = 0;
+
+        for(int i = 0; i < height; i++) {
+
+            System.out.println("-----------Iteracion "+(i+1)+"------------------");
+            actualPositionX = ((int)Math.pow(2, height-1-i) - 1)*circleSize;
+            System.out.println("Altura -> "+i);
+            System.out.println("Ancho -> "+width);
+            System.out.println("actualPositionX -> "+actualPositionX);
+            System.out.println("Multiplo de CircleSize -> "+actualPositionX/circleSize);
+
+            // Print tree node elements
+            for(Nodo n : current) {
+
+                System.out.println("************Iteracion Interna****************");
+                System.out.println("CircleSize -> "+(circleSize*Math.round(width / (acum + 1))));
+                System.out.println("Multiplo CircleSize -> "+(Math.round(width / (acum + 1))));
+                System.out.println("Valor de acum -> "+acum);
+                System.out.println("Valor multiplo de PositionX -> "+(actualPositionX/circleSize));
+                System.out.println("Valor actual de PositionX -> "+(actualPositionX));
+
+                if(n == null) {
+                    next.add(null);
+                    next.add(null);
+
+                } else {
+
+                    drawNodeCircle(n.getElemento().toString(), actualPositionX, actualPositionY);
+                    next.add(n.getIzquierdo());
+                    next.add(n.getDerecho());
+                }
+
+                actualPositionX += circleSize*Math.round(width / (acum + 1));
+
+                System.out.println("*********************************************");
+
+            }
+
+            if(i>0) acum++;
+
+            actualPositionY+=circleSize*2;
+
+            elements *= 2;
+            current = next;
+            next = new ArrayList<>(elements);
+
+            System.out.println("------------------------------------------------");
+
+
+        }
+
+
+    }
+
+}
